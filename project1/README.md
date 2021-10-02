7aeb3f742fcf5642a9bd46721220f51e49622adc
daveschloemer <schloede@mail.uc.ed>
2021-10-02 12:10:35 -0400

    project1

--- project1/README.md
+++ project1/README.md
@@ -2,3 +2,9 @@
 
 This is David Schloemer's first project
 
+This project was written to find three keywords in a massive text file. The three words are Cherry, Peach,
+and Apple. All three are hidden throughout the text making it impossible to find all of them. This program
+will sort and show each line where those three keywords are. I wrote this code entirely on my own.
+
+To get started, simply click the run button!
+
---
+++ project1/fruitfinder.ps1
@@ -0,0 +1,12 @@
+﻿cd C:\it3038c-scripts\project1 ##Change directory to repository
+
+$files = Get-ChildItem  ##Gets content from project1.txt and puts it into files variable
+
+
+
+foreach ($file in $files) {        ##Searches for Cherry, Apple, and Peach in the massive text file, and then outputs which line they are on
+    $content = Get-Content $file
+    $content.Split("*") | Select-String Cherry, Apple, Peach | Select-Object Line
+}
+
+##All of this was written on my own. I did ask for help regarding a foreach loop
