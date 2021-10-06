8c9408eeef13d2c53a7bcaef8838bc68613012e8
daveschloemer <schloede@mail.uc.ed>
2021-10-05 21:08:31 -0400

    main

--- node/getIPByHostname.js
+++ node/getIPByHostname.js
@@ -1,16 +1,16 @@
 var dns = require('dns');
 
 function hostnameLookup(hostname) {
-    dns.lookup(hostname, function(err, addresses, family){
-        console.log(addresses);
-    });
+   dns.lookup(hostname, function(err, addresses, family){
+      console.log(addresses);
+   });
 }
 
-if (process.argv.length < 3) {
-    console.log("USAGE: " + __filename + "hostname.com")
-    process.exit(-1)
+if (process.argv.length <= 2) {
+        console.log("USAGE: " + __filename + " IPADDR")
+        process.exit(-1)
 }
 
 var hostname = process.argv[2]
 console.log(`Checking IP of: ${hostname}`)
-hostnameLookup(hostname)
+hostnameLookup(hostname);
