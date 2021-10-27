afe3fd87e7ad377d4cad0255624c3d90018c92a1
daveschloemer <schloede@mail.uc.ed>
2021-10-27 13:59:14 -0400

    project 2 10/27/2021

---
+++ project 2/wordfilter.ps1
@@ -0,0 +1,12 @@
+Get-ChildItem -Path (Read-Host -Prompt "Where is this file stored?") ##Change directory to repository where the user requested
+
+$files = Get-ChildItem  ##Gets content from project1.txt and puts it into files variable
+
+$userInput = Read-Host "What are you looking for in this file?"
+
+foreach ($file in $files) {        ##Searches for Cherry, Apple, and Peach in the massive text file, and then outputs which line they are on
+    $content = Get-Content $file
+    $content.Split("*") | Select-String $userInput | Select-Object Line
+}
+
+##All of this was written on my own. I did ask for help regarding a foreach loop
